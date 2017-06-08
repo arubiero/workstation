ea9ceaeaeca8280216a733f2a1355acf4d54b350
Adrian Rubiero <adrian@rubiero.com>
2017-06-08 19:38:05 +0200

    creados test de integracion

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,4 +1,10 @@
+#yum_repository "epel"do
+#  baseurl 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
+#  action :create
+#end
+
 package "tree"
+#package "cowsay"
 
 file "/etc/motd" do
   content "Property of Adrian\n"
